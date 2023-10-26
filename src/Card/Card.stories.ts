d1d67fee9fb890e985a4a4c5be891be931cec94f
Gabriel Quiles <gaby.quiles@gmail.com>
2023-10-25 23:05:46 -0400

    Polish Story

--- src/Card/Card.stories.ts
+++ src/Card/Card.stories.ts
@@ -6,7 +6,14 @@ import { Card } from './Card';
 
 //👇 This default export determines where your story goes in the story list
 const meta: Meta<typeof Card> = {
+  title: 'Atoms/Card',
   component: Card,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'centered',
+  },
 };
 
 export default meta;
